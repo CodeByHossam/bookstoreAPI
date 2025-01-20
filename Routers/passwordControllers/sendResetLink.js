d18c2ae524c4de2d6b853db547021d4ffdfedb30
CodeByHossam <eng.hossam00@gmail.com>
2025-01-20 22:28:35 +0400

    adding feature to send the reset password link by email

--- Routers/passwordControllers/sendResetLink.js
+++ Routers/passwordControllers/sendResetLink.js
@@ -1,5 +1,6 @@
 const { User } = require("../../Models/User");
 const jwt = require("jsonwebtoken");
+const sendEmail = require("../../MiddelWare/emailSender")
 
 const sendResetLink = async (req, res) => {
   try {
@@ -12,7 +13,8 @@ const sendResetLink = async (req, res) => {
     const token = jwt.sign({ email: user.email, id: user.id }, secret);
     const link = `http://localhost:5000/password/reset-password/${user.id}/${token}`;
 
-    return res.status(200).send(`
+    // Construct the HTML content for the email
+    const htmlWithLink = `
       <html>
       <body style="background-color: rgb(9, 53, 81); color: white; font-family: Arial, sans-serif; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh;">
           <div style="text-align: center; padding: 20px; margin: 20px; border-radius: 10px; background-color: #1e3a5f;">
@@ -20,7 +22,12 @@ const sendResetLink = async (req, res) => {
           </div>
       </body>
       </html>
-    `);
+    `;
+
+    // Send the email
+    sendEmail(user.email, htmlWithLink);
+
+    return res.status(200).render("resetLinkSent", { email: user.email });
   } catch (error) {
     console.error("Error sending reset link:", error);
     res.status(500).send("Internal server error");
